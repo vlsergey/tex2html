173e88aef812f5c0d40c5939f8f9cf4b3a70ee30
Sergey Vladimirov <vlsergey@gmail.com>
2021-12-21 16:26:01 +0300

    Support numeration of subsections

--- src/main/java/com/github/vlsergey/tex2html/processors/NumerationProcessor.java
+++ src/main/java/com/github/vlsergey/tex2html/processors/NumerationProcessor.java
@@ -28,6 +28,7 @@ public class NumerationProcessor implements TexXmlProcessor {
 				xmlDoc, XPathConstants.BOOLEAN);
 		final MutableInt chapterCounter = new MutableInt(0);
 		final MutableInt sectionCounter = new MutableInt(0);
+		final MutableInt subsectionCounter = new MutableInt(0);
 
 		final MutableInt figureCounter = new MutableInt(0);
 
@@ -43,6 +44,7 @@ public class NumerationProcessor implements TexXmlProcessor {
 			if (TexXmlUtils.isCommandElement(node, "chapter")) {
 				chapterCounter.increment();
 				sectionCounter.setValue(0);
+				subsectionCounter.setValue(0);
 
 				final Element chapter = (Element) node;
 				chapter.setAttribute("chapter-index", chapterCounter.toString());
@@ -52,12 +54,27 @@ public class NumerationProcessor implements TexXmlProcessor {
 
 			if (TexXmlUtils.isCommandElement(node, "section")) {
 				sectionCounter.increment();
+				subsectionCounter.setValue(0);
 
 				final Element section = (Element) node;
 				section.setAttribute("level", "1");
 				section.setAttribute("section-index", sectionCounter.toString());
 				section.setAttribute("index", haveChapters ? chapterCounter.toString() + "." + sectionCounter.toString()
 						: sectionCounter.toString());
+			}
+
+			if (TexXmlUtils.isCommandElement(node, "subsection")) {
+				subsectionCounter.increment();
+
+				final Element subsection = (Element) node;
+				subsection.setAttribute("level", "2");
+				subsection.setAttribute("section-index", sectionCounter.toString());
+				subsection.setAttribute("subsection-index", subsectionCounter.toString());
+				subsection.setAttribute("index",
+						haveChapters
+								? chapterCounter.toString() + "." + sectionCounter.toString() + "."
+										+ subsectionCounter.toString()
+								: sectionCounter.toString() + "." + subsectionCounter.toString());
 			}
 
 			if (TexXmlUtils.isCommandElement(node, "figure")) {
